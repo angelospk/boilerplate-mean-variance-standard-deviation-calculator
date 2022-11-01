59b9a76bf634fa2ae5331fac8bd33d156cd1bb91
Angelos Papamichail <55174356+angelospk@users.noreply.github.com>
2022-11-01 14:41:55 +0000

    completed exercise

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,15 @@
 import numpy as np
 
 def calculate(list):
-
-
-
-
-    return calculations
+  if (len(list)<9):
+    raise ValueError("List must contain nine numbers.")
+  ls=np.array(list).reshape(3,3)
+  ret={"mean":[ls.mean(axis=0).tolist(),ls.mean(axis=1).tolist(),np.mean(ls).tolist()],
+  "variance":[ls.var(axis=0).tolist(),ls.var(axis=1).tolist(),np.var(ls).tolist()],
+  "standard deviation":[ls.std(axis=0).tolist(),ls.std(axis=1).tolist(),np.std(ls).tolist()],
+  "max":[ls.max(axis=0).tolist(),ls.max(axis=1).tolist(),np.max(ls).tolist()],
+  "min":[ls.min(axis=0).tolist(),ls.min(axis=1).tolist(),np.min(ls).tolist()],
+  "sum":[ls.sum(axis=0).tolist(),ls.sum(axis=1).tolist(),np.sum(ls).tolist()],
+  }
+  
+  return ret
